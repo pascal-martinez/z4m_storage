bd915ab0403767fea8c2ae883ee002f78b7c928a
Pascal MARTINEZ <contact@znetdk.fr>
2025-01-04 22:14:41 +0100

    CHANGE: version 1.1 of the module

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,9 @@
 # CHANGE LOG: Storage (z4m_storage)
 
+## Version 1.1, 2024-12-23
+- BUG FIXING: the PHP constant MOD_Z4M_STORAGE_PHOTOS_REMOVE_QUESTION was not used and so was removed.
+- BUG FIXING: translations of confirmation and notification messages are now generic and use the term "file" instead of "photo" or "document".
+- BUG FIXING: in PHP 8.3, error "E_DEPRECATED - Calling get_parent_class() without arguments is deprecated - ... DocumentDAO.php(48)".
+
 ## Version 1.0, 2024-12-08
 First version.
--- mod/config.php
+++ mod/config.php
@@ -18,8 +18,8 @@
  * --------------------------------------------------------------------
  * Parameters of the ZnetDK 4 Mobile Storage module
  *
- * File version: 1.0
- * Last update: 11/30/2024
+ * File version: 1.1
+ * Last update: 01/03/2025
  */
 
 /**
@@ -70,9 +70,9 @@ define('MOD_Z4M_STORAGE_SQL_SCRIPT_PATH', ZNETDK_MOD_ROOT
  * Module version number
  * @return string Version
  */
-define('MOD_Z4M_STORAGE_VERSION_NUMBER','1.0');
+define('MOD_Z4M_STORAGE_VERSION_NUMBER','1.1');
 /**
  * Module version date
  * @return string Date in W3C format
  */
-define('MOD_Z4M_STORAGE_VERSION_DATE','2024-11-30');
+define('MOD_Z4M_STORAGE_VERSION_DATE','2025-01-03');
